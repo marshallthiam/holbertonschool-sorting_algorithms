7e036ae550fa725409edd40824e7e1bfb8371a79
marshallthiam <marshallthiam@holbertonstudents.com>
2025-06-26 11:46:55 +0000

    Bubble sort

--- sorting_algorithms/0-bubble_sort.c
+++ sorting_algorithms/0-bubble_sort.c
@@ -2,37 +2,39 @@
 #include <stdio.h>
 
 /**
- * bubble_sort - Sorts an array of integers in ascending order
- *               using the Bubble sort algorithm
- * @array: Array of integers to sort
- * @size: Size of the array
+ * bubble_sort - Trie un tableau d'entiers en ordre croissant
+ *               avec l'algorithme Bubble sort
+ * @array: Le tableau d'entiers à trier
+ * @size: La taille du tableau
  */
 void bubble_sort(int *array, size_t size)
 {
-	size_t i, j;
-	int temp;
-	int swapped;
+    size_t i, j;
+    int tmp;
+    int swapped;
 
-	if (array == NULL || size < 2)
-		return;
+    if (array == NULL || size < 2)
+        return;
 
-	for (i = 0; i < size - 1; i++)
-	{
-		swapped = 0;
-		for (j = 0; j < size - 1 - i; j++)
-		{
-			if (array[j] > array[j + 1])
-			{
-				/* Swap */
-				temp = array[j];
-				array[j] = array[j + 1];
-				array[j + 1] = temp;
+    for (i = 0; i < size - 1; i++)
+    {
+        swapped = 0;
 
-				print_array(array, size);
-				swapped = 1;
-			}
-		}
-		if (!swapped)
-			break;
-	}
+        for (j = 0; j < size - i - 1; j++)
+        {
+            if (array[j] > array[j + 1])
+            {
+                /* Échange */
+                tmp = array[j];
+                array[j] = array[j + 1];
+                array[j + 1] = tmp;
+
+                print_array(array, size);  // 📌 Impression après chaque swap
+                swapped = 1;
+            }
+        }
+
+        if (!swapped)
+            break;
+    }
 }
