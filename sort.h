0747dde5d38c6c03e1387c6ed8ab13ad52560d3b
marshallthiam <marshallthiam@holbertonstudents.com>
2025-07-15 14:01:05 +0000

    sort.h

--- sort.h
+++ sort.h
@@ -1,10 +1,9 @@
 #ifndef SORT_H
 #define SORT_H
 
-#include <stdio.h>
-#include <stdlib.h>
+#include <stddef.h>
 
+void selection_sort(int *array, size_t size);
 void print_array(const int *array, size_t size);
-void quick_sort(int *array, size_t size);
 
 #endif /* SORT_H */
