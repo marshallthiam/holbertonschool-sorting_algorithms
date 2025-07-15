644d83e9bce4843c088307ca4ce551a8c67fcc9c
marshallthiam <marshallthiam@holbertonstudents.com>
2025-07-15 13:54:04 +0000

    Selection sort

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -1,35 +1,34 @@
-#include <stdio.h>
-#include  <sort.h>
+#include "sort.h"
 
 /**
- * print_array - prints an array of integers
- * @array: array to print
- * @size: size of the array
+ * selection_sort - Sorts an array using the Selection sort algorithm
+ * @array: The array of integers to sort
+ * @size: The number of elements in the array
  */
-void print_array(const int *array, size_t size);
-
 void selection_sort(int *array, size_t size)
 {
-    size_t i, j, min_idx;
-    int temp;
+	size_t i, j, min_idx;
+	int tmp;
 
-    if (array == NULL || size < 2)
-        return;
+	if (array == NULL || size < 2)
+		return;
 
-    for (i = 0; i < size - 1; i++)
-    {
-        min_idx = i;
-        for (j = i + 1; j < size; j++)
-        {
-            if (array[j] < array[min_idx])
-                min_idx = j;
-        }
-        if (min_idx != i)
-        {
-            temp = array[i];
-            array[i] = array[min_idx];
-            array[min_idx] = temp;
-            print_array(array, size);
-        }
-    }
+	for (i = 0; i < size - 1; i++)
+	{
+		min_idx = i;
+
+		for (j = i + 1; j < size; j++)
+		{
+			if (array[j] < array[min_idx])
+				min_idx = j;
+		}
+
+		if (min_idx != i)
+		{
+			tmp = array[i];
+			array[i] = array[min_idx];
+			array[min_idx] = tmp;
+			print_array(array, size);
+		}
+	}
 }
